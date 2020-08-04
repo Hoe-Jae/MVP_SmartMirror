833d0719c94ac2bd0ea74dccbb008dd0a9fb883b
Hoe-Jae <60731875+Hoe-Jae@users.noreply.github.com>
2020-08-04 16:50:55 +0900

    MVP_JS

---
+++ js/main.js
@@ -0,0 +1,32 @@
+(function ($){
+
+    /*------------------
+        Product Slider
+    --------------------*/
+    $(".product-slider").owlCarousel({
+        loop: true,
+        margin: 25,
+        nav: true,
+        items: 6,
+        dots: true,
+        navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
+        smartSpeed: 200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            992: {
+                items: 2,
+            },
+            1200: {
+                items: 3,
+            }
+        }
+    })
+
+})(jQuery);
